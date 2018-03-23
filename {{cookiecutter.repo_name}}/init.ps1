a1faf9665baec6b378336823f7ad458d5825ff90
mruiz <mruiz019@gmail.com>
2018-03-23 12:03:57 -0700

    added a powershell script for git setup, good for my current local setup, can be updated to be more versatile

---
+++ {{cookiecutter.repo_name}}/init.ps1
@@ -0,0 +1,20 @@
+# setup git repo
+$local_repo = "$env:dev\libguides\gitbooks\{{ cookiecutter.repo_name }}"
+
+# git setup
+cd $local_repo
+
+# initialize
+git init
+
+# add remote
+
+git remote add origin {{ cookiecutter.github_repo_ssh }}
+
+# add and commit
+git add *
+
+git commit -am "initial commit of {{ cookiecutter.project_name }} project"
+
+# push (remmber can only push if using an ssh url and you are a collaborator)
+git push -u origin master
